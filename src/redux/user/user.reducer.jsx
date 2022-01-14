90b0ec2f0c12fa5cc581261bb95d28a8c9860d60
Rupinder <rupinder3972@gmail.com>
2022-01-14 13:19:42 +0530

    redirection to home if user is logged in

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,10 +1,11 @@
+import {userActionTypes} from "./user.type"
 const INTIAL_STATE = {
     currentUser: null
 }
 
 const userReducer = (state = INTIAL_STATE, action) => {
     switch(action.type) {
-        case 'SET_CURRENT_USER':
+        case userActionTypes.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload
