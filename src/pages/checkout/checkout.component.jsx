19a51df0f9abd4dcdb1ed9188a6c02f008c56ff2
Rupinder <rupinder3972@gmail.com>
2022-01-14 18:13:09 +0530

    added functionality for add/remove items at checout

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -23,9 +23,7 @@ const CheckoutPage = ({cartItems, total}) => (
                 <span>Remove</span>
             </div>
         </div>
-        {
-            cartItems.map(cartItem => (<CheckoutItem key={cartItem.id} cartItem={cartItem} />))
-        }
+        {cartItems.map(cartItem => (<CheckoutItem key={cartItem.id} cartItem={cartItem} />))}
         <div className="total">
             <span>${total}</span>
         </div>
